c0adb46098e9518ebcba3b71aea60a5a3a98a7a3
Igor S. Morozov <igor@morozov.is>
2022-01-18 00:23:44 +0300

    Bring back the host/port

--- config/runtime.exs
+++ config/runtime.exs
@@ -40,10 +40,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :deezer_family_playlist, DeezerFamilyPlaylistWeb.Endpoint,
-    url: [port: 443],
+    url: [host: host, port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
